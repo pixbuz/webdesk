7ba1dace59d1e6a02db2c2c05680a5a0db01a24b
Pizzux <139716243+FatiguedFrench@users.noreply.github.com>
2025-11-13 21:11:58 +0000

    Basic Get App Manifests API

--- backend/main.ts
+++ backend/main.ts
@@ -4,21 +4,67 @@ Deno.serve({
 	handler: Server,
 	onListen({ port, hostname }) {
 		console.log(`Backend Ready at ${hostname}:${port}`);
-	},
-});
+	}
+})
 
-function Server(UserRequest: Request): Response {
+interface ResponseFormat {
+	message: unknown,
+	responseFunction: string,
+	returnedError: boolean
+}
+
+interface Manifest {
+	name: string,
+	index: string
+}
+
+async function Server(UserRequest: Request): Promise<Response> {
 	const RequestURL = new URL(UserRequest.url)
+	let Reply: ResponseFormat = { message: null, responseFunction: "", returnedError: false }
 
 	switch (RequestURL.pathname) {
-		case "/api":
 		case "/api/":
-			return new Response("This is the Webdesk Backend!");
+		case "/api": Reply.message = "This is the Webdesk Backend!"; break
 
-		case "/api/getAppManifests":
 		case "/api/getAppManifests/":
-			return new Response("g")
+		case "/api/getAppManifests": await getAppManifests(Reply); break
 
-		default: return new Response(`${RequestURL.pathname} is not mapped to any command`);
+		default: 
+			Reply.message = `${RequestURL.pathname} is not mapped to any command`;
+			Reply.returnedError = true
+	}
+
+	return new Response(JSON.stringify(Reply), {
+		status: (Reply.returnedError ? 400 : 200),
+		headers: { "Content-Type": "application/json" }
+	})
+}
+
+async function getAppManifests(Reply: ResponseFormat): Promise<ResponseFormat> {
+	Reply.responseFunction = "getAppManifests"
+	const ApplicationManifests: string[] = [];
+	let WaitNum = 0
+
+	try {
+		for await (const Manifest of Deno.readDirSync("./applications/manifests")) {
+			WaitNum++;
+			Deno.readTextFile(`./applications/manifests/${Manifest.name}`)
+				.then(ManifestText => ApplicationManifests.push(JSON.parse(ManifestText)))
+		}
+
+		while (ApplicationManifests.length != WaitNum) 
+			await new Promise<void>(r => setTimeout(r))
+
+		Reply.message = ApplicationManifests
+	} catch(error) {
+		Reply.returnedError = true
+		Reply.message = JSON.stringify(error)
+	} finally {
+		// deno-lint-ignore no-unsafe-finally
+		return Reply 
 	}
 }
+
+console.log("===================================================")
+console.log(`Deno Current Working Directory: ${Deno.cwd()}`)
+console.log("===================================================")
